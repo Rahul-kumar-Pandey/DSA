c2759d4da5768656d5b181b0d0b07130bbd24b49
Rahul Kumar Pandey <77538550+Rahul-kumar-Pandey@users.noreply.github.com>
2025-06-24 09:47:35 +0530

    Create 1:medicanofRowWiseSortedMatrix.cpp

---
+++ 9:BinarySearch/1:medicanofRowWiseSortedMatrix.cpp
@@ -0,0 +1,56 @@
+/*
+Given a row-wise sorted matrix where the number of rows and columns is always odd, find the median of the matrix.
+Input: mat = [[1, 3, 5], [2, 6, 9], [3, 6, 9]]
+Output: 5
+*/
+/* Optimal Approcah:(Binary Search)
+  1)lo=min(mat),hi=max(mat) , then find mid . 
+  2)for mid how many elements are smaller than it and store answer;
+  Time Complexity: O(n log m * log(maxVal – minVal))
+  Auxiliary Space: O(1)
+*/
+int upperBound(vector<int> &arr, int x, int n) {
+    int low = 0, high = n - 1;
+    int ans = n;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        // maybe an answer
+        if (arr[mid] > x) {
+            ans = mid;
+            //look for smaller index on the left
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1; // look on the right
+        }
+    }
+    return ans;
+}
+
+int countSmallEqual(vector<vector<int>> &matrix, int m, int n, int x) {
+    int cnt = 0;
+    for (int i = 0; i < m; i++) {
+        cnt += upperBound(matrix[i], x, n);
+    }
+    return cnt;
+}
+
+int median(vector<vector<int>> &matrix, int m, int n) {
+    int low = INT_MAX, high = INT_MIN;
+
+    //point low and high to right elements:
+    for (int i = 0; i < m; i++) {
+        low = min(low, matrix[i][0]);
+        high = max(high, matrix[i][n - 1]);
+    }
+
+    int req = (n * m) / 2;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int smallEqual = countSmallEqual(matrix, m, n, mid);
+        if (smallEqual <= req) low = mid + 1;
+        else high = mid - 1;
+    }
+    return low;
+}
