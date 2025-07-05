b0db07ccf2d780d729ae8bb5fd1f43f1a82282b9
Rahul Kumar Pandey <77538550+Rahul-kumar-Pandey@users.noreply.github.com>
2025-07-05 19:46:57 +0530

    Update 1.createstack.cpp

--- stack&queues/1.createstack.cpp
+++ stack&queues/1.createstack.cpp
@@ -30,3 +30,11 @@ int main(){
   s.push(7);
   return 0;
 }
+//Stack using one queue
+/*Approach:
+Take a single queue.
+push(x): Push the element in the queue.
+Use a for loop of size()-1, remove element from queue and again push back to the queue, hence the most recent element becomes the most former element and vice versa.
+pop(): remove the element from the queue.
+top(): show the element at the top of the queue.
+size(): size of the current queue.*/
