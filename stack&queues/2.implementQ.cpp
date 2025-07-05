45efd503f70d0e6b7e69eddce4e8cc4c5d782514
Rahul Kumar Pandey <77538550+Rahul-kumar-Pandey@users.noreply.github.com>
2025-07-05 19:55:17 +0530

    Update 2.implementQ.cpp

--- stack&queues/2.implementQ.cpp
+++ stack&queues/2.implementQ.cpp
@@ -54,3 +54,83 @@ class Queue {
     return currSize;
   }
 };
+---------------------------------------------------------------------------------------
+//Implement Queue using Stack
+//Solution 1: Using two Stacks where push operation is O(N)
+struct Queue {
+  stack < int > input, output;
+  
+  // Push elements in queue
+  void Push(int data) {
+    // Pop out all elements from the stack input
+    while (!input.empty()) {
+      output.push(input.top());
+      input.pop();
+    }
+    // Insert the desired element in the stack input
+    cout << "The element pushed is " << data << endl;
+    input.push(data);
+    // Pop out elements from the stack output and push them into the stack input
+    while (!output.empty()) {
+      input.push(output.top());
+      output.pop();
+    }
+  }
+  // Pop the element from the Queue
+  int Pop() {
+    if (input.empty()) {
+      cout << "Stack is empty";
+      exit(0);
+    }
+    int val = input.top();
+    input.pop();
+    return val;
+  }
+  // Return the Topmost element from the Queue
+  int Top() {
+    if (input.empty()) {
+      cout << "Stack is empty";
+      exit(0);
+    }
+    return input.top();
+  }
+  // Return the size of the Queue
+  int size() {
+    return input.size();
+  }
+};
+------------------------------------------------------------------------------------
+//Solution 2: Using two Stacks where push operation is O(1)
+class MyQueue {
+  public:
+    stack < int > input, output;
+  
+  /** Push element x to the back of queue. */
+  void push(int x) {
+    cout << "The element pushed is " << x << endl;
+    input.push(x);
+  }
+
+  /** Removes the element from in front of queue and returns that element. */
+  int pop() {
+    // shift input to output 
+    if (output.empty())
+      while (input.size())
+        output.push(input.top()), input.pop();
+
+    int x = output.top();
+    output.pop();
+    return x;
+  }
+  /** Get the front element. */
+  int top() {
+    // shift input to output 
+    if (output.empty())
+      while (input.size())
+        output.push(input.top()), input.pop();
+    return output.top();
+  }
+  int size() {
+    return (output.size() + input.size()); 
+  }
+};
