1a16db983ec6a82d01f78ef45e5341d34c986c33
Rahul Kumar Pandey <77538550+Rahul-kumar-Pandey@users.noreply.github.com>
2025-07-08 00:32:06 +0530

    Update LFU.cpp

--- stack&queues/LFU.cpp
+++ stack&queues/LFU.cpp
@@ -11,3 +11,101 @@ IDEA:
 ## in dll it is stored in LRU manner
 ## map<key,Node> to retreive fast key , value
 */
+
+struct Node{
+    int key;
+    int value;
+    int cnt;
+    Node* prev;
+    Node* next;
+    Node(int _key,int _value){
+        cnt=1;
+        this->key=_key;
+        this->value=_value;
+    }
+};
+struct DLL{
+    Node* tail;
+    Node* head;
+    int size;
+    DLL(){
+        head=new Node(0,0);
+        tail=new Node(0,0);
+        head->next=tail;
+        tail->prev=head;
+        size=0;
+    }
+    void addFront(Node * node){
+        Node* headnxt=head->next;
+        head->next=node;
+        node->prev=head;
+        node->next=headnxt;
+        headnxt->prev=node;
+        size++;
+    }
+    void removeNode(Node* node){
+        node->prev->next=node->next;
+        node->next->prev=node->prev;
+        size--;
+    }
+};
+class LFUCache {
+    map<int,Node*>keymp;
+    map<int,DLL*>freqmp;
+    int minfreq;
+    int currsize;
+    int maxsize;
+public:
+    LFUCache(int capacity) {
+        maxsize=capacity;
+        minfreq=0;
+        currsize=0;
+    }
+    void update(Node* node){
+        freqmp[node->cnt]->removeNode(node);
+        if(minfreq==node->cnt && freqmp[node->cnt]->size==0){
+            minfreq++;
+        }
+        DLL * nxtHigherfreqlist=new DLL();
+        if(freqmp.find(node->cnt+1)!=freqmp.end()){
+            nxtHigherfreqlist=freqmp[node->cnt+1];
+        }
+        node->cnt=node->cnt+1;
+        nxtHigherfreqlist->addFront(node);
+        freqmp[node->cnt]=nxtHigherfreqlist;
+    }
+    int get(int key) {
+        if(keymp.find(key)!=keymp.end()){
+            update(keymp[key]);
+            return keymp[key]->value;
+        }
+        return -1;
+    }
+    
+    void put(int key, int value) {
+        //key already exists>> update
+        if(keymp.find(key)!=keymp.end()){
+            Node * node=keymp[key];
+            node->value=value;
+            update(node);
+        }
+        else{
+            if(keymp.size()==maxsize){
+                //delete least frequent
+                DLL* list=freqmp[minfreq];
+                keymp.erase(list->tail->prev->key);
+                list->removeNode(list->tail->prev);
+            }
+            //add new key value;
+            minfreq=1;
+            DLL* newList=new DLL();
+            if(freqmp.find(minfreq)!=freqmp.end()){
+                newList=freqmp[minfreq];
+            }
+            Node * node=new Node(key,value);
+            newList->addFront(node);
+            keymp[key]=node;
+            freqmp[minfreq]=newList;
+        }
+    }
+};
