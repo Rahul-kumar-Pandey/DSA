cfc4a26746c4fa1a47e18942d07139098e5c35cd
Rahul Kumar Pandey <77538550+Rahul-kumar-Pandey@users.noreply.github.com>
2025-07-21 22:57:18 +0530

    Update 2.views.cpp

--- Trees/2.views.cpp
+++ Trees/2.views.cpp
@@ -25,3 +25,37 @@ vector<int> bottomView(Node* root){
         }       
         return ans;
     }
+
+//Top view of Binary Tree
+/*
+1) we can use similar method as for previous problem
+2) but this will cost nlogn because of map,
+3) so we will use unordered_map to reduce complexity to O(N)
+*/
+if (root ==  nullptr) {
+        return {};
+    }
+    queue<pair<Node *, int>> q;
+    unordered_map<int, int> mp;
+    int mn = INT_MAX;
+    q.push({root, 0});
+    while (!q.empty()) { 
+        temp = q.front().first;
+        int d = q.front().second;
+        mn = min(mn, d); //this will store left bound
+        q.pop();
+        if (mp.find(d) == mp.end()) {
+            mp[d] = temp->data;
+        }
+        if (temp->left) {
+            q.push({temp->left, d - 1});
+        }
+        if (temp->right) {
+            q.push({temp->right, d + 1});
+        }
+    }
+    vector<int> ans(mp.size());
+    for (auto it = mp.begin(); it != mp.end(); it++) {
+        ans[it->first - mn] = (it->second);        //we are shifting index 
+    }
+    return ans;
