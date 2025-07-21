857d5a3f02170a2e7a366892a5c74571a4b2dff5
Rahul Kumar Pandey <77538550+Rahul-kumar-Pandey@users.noreply.github.com>
2025-07-21 23:07:59 +0530

    Update 1)Traversals.cpp

--- Trees/1)Traversals.cpp
+++ Trees/1)Traversals.cpp
@@ -110,3 +110,34 @@ while(curr){
 }
 
 //NOTE: for preorder just place line no 106(print statement) after line no 100.
+------------------------------------------------------------------------------------------------
+//inorder,preorder,postorder in a single traversal
+    vector<int> pre, in, post;
+    if (root == NULL) {
+        return {};
+    }
+    stack<pair<Node*, int>> st;
+    st.push({root, 1});
+    while (!st.empty()) {
+        auto it = st.top();
+        st.pop();
+        if (it.second == 1) {
+            pre.push_back(it.first->data);
+            it.second = 2;
+            st.push(it); 
+            if (it.first->left != NULL) {
+                st.push({it.first->left, 1});
+            }
+        }
+        else if (it.second == 2) {
+            in.push_back(it.first->data);
+            it.second = 3;
+            st.push(it); 
+            if (it.first->right != NULL) {
+                st.push({it.first->right, 1});
+            }
+        }
+        else {
+            post.push_back(it.first->data);
+        }
+    }
