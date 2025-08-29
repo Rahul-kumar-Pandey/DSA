9a656f606ddd96d03b8976fde17a92cb87d8ffd3
Rahul Kumar Pandey <77538550+Rahul-kumar-Pandey@users.noreply.github.com>
2025-08-29 08:38:59 +0530

    Create 2.MST.cpp

---
+++ GRAPH/MSTDSU/2.MST.cpp
@@ -0,0 +1,10 @@
+/*
+##spanning Tree=>
+Given an undirected and connected graph 
+, a spanning tree of the graph 
+ is a tree that spans 
+ (that is, it includes every vertex of G) and is a subgraph of G 
+ (every edge in the tree belongs to G)
+
+ ##Minimum spanning tree=> The spanning Tree where the cost is minimum amoung all the spanning Trees.
+*/
