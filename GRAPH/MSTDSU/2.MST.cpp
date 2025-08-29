2a4223b06628f9dd1fd799a5336086fae411078a
Rahul Kumar Pandey <77538550+Rahul-kumar-Pandey@users.noreply.github.com>
2025-08-29 09:43:00 +0530

    Update 2.MST.cpp

--- GRAPH/MSTDSU/2.MST.cpp
+++ GRAPH/MSTDSU/2.MST.cpp
@@ -8,3 +8,35 @@ Given an undirected and connected graph
 
  ##Minimum spanning tree=> The spanning Tree where the cost is minimum amoung all the spanning Trees.
 */
+//----------------------------PRIM's ALGORITHM--------------------------
+//preparation:minheap, visited array, mst array(optional) which stores mst edges
+//minheap stores {wt,node}
+int spanningTree(int V, vector<vector<int>> adj[])
+	{
+		priority_queue<pair<int, int>,
+		               vector<pair<int, int> >, greater<pair<int, int>>> pq;
+
+		vector<int> vis(V, 0);
+		// {wt, node}
+		pq.push({0, 0});
+		int sum = 0;
+		while (!pq.empty()) {
+			auto it = pq.top();
+			pq.pop();
+			int node = it.second;
+			int wt = it.first;
+
+			if (vis[node] == 1) continue;
+			// add it to the mst
+			vis[node] = 1;
+			sum += wt;
+			for (auto it : adj[node]) {
+				int adjNode = it[0];
+				int edW = it[1];
+				if (!vis[adjNode]) {
+					pq.push({edW, adjNode});
+				}
+			}
+		}
+		return sum;
+	}
