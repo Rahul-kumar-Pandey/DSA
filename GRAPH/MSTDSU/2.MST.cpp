c2fab1de08e94323f8629b46ad23751e9d180a30
Rahul Kumar Pandey <77538550+Rahul-kumar-Pandey@users.noreply.github.com>
2025-09-02 10:25:21 +0530

    Update 2.MST.cpp

--- GRAPH/MSTDSU/2.MST.cpp
+++ GRAPH/MSTDSU/2.MST.cpp
@@ -41,3 +41,38 @@ int spanningTree(int V, vector<vector<int>> adj[])
 		}
 		return sum;
 	}
+
+//kruskal's algorithm
+/*
+INtution->1) sort the edges by their weight
+		  2) use concept of disjoint set to join verices
+*/
+  int spanningTree(int V, vector<vector<int>> adj[])
+    {
+        vector<pair<int, pair<int, int>>> edges;
+        for (int i = 0; i < V; i++) {
+            for (auto it : adj[i]) {
+                int adjNode = it[0];
+                int wt = it[1];
+                int node = i;
+
+                edges.push_back({wt, {node, adjNode}});
+            }
+        }
+        DisjointSet ds(V);
+        sort(edges.begin(), edges.end());
+        int mstWt = 0;
+        for (auto it : edges) {
+            int wt = it.first;
+            int u = it.second.first;
+            int v = it.second.second;
+
+            if (ds.findUPar(u) != ds.findUPar(v)) {
+                mstWt += wt;
+                ds.unionBySize(u, v);
+            }
+        }
+
+        return mstWt;
+    }
+//Overall Time Complexity: (O(Elog E)) or (O(Elog V))
